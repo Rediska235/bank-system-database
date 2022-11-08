eebde35aff37cfddb7410398e75d7f11a937612d
rediska235 <vanya.bobr.12@gmail.com>
2022-11-08 14:42:26 +0300

    Add task 2, 3, 4

--- bank system.sql
+++ bank system.sql
@@ -1,3 +1,4 @@
+/* database creation */
 CREATE DATABASE bank_system;
 GO
 
@@ -30,6 +31,7 @@ GO
 
 CREATE TABLE clients(
     id INT PRIMARY KEY IDENTITY,
+    status_id INT NOT NULL,
     full_name VARCHAR(30) NOT NULL
 );
 GO
@@ -49,6 +51,7 @@ CREATE TABLE bank_cards(
 );
 GO
 
+/* database filling */
 INSERT INTO cities (city_name)
 VALUES ('Минск'),
        ('Витебск'),
@@ -83,12 +86,12 @@ VALUES ('Пенсионер'),
        ('Рабочий');
 GO
 
-INSERT INTO clients (full_name)
-VALUES ('Мельников Аполлон Владимирович'),
-       ('Ковалёв Максим Федорович'),
-       ('Доронин Леонид Васильевич'),
-       ('Карпов Артур Андреевич'),
-       ('Муравьёв Александр Авдеевич');
+INSERT INTO clients (status_id, full_name)
+VALUES (5, 'Мельников Аполлон Владимирович'),
+       (1, 'Ковалёв Максим Федорович'),
+       (5, 'Доронин Леонид Васильевич'),
+       (2, 'Карпов Артур Андреевич'),
+       (4, 'Муравьёв Александр Авдеевич');
 GO
 
 INSERT INTO accounts (bank_id, client_id, balance)
@@ -105,4 +108,40 @@ VALUES (1, 130),
        (1, 43),
        (3, 97),
        (4, 27);
+GO
+
+/* task 2
+Покажи мне список банков у которых есть филиалы в городе X (выбери один из городов)
+*/
+
+SELECT bank_name
+FROM banks
+    JOIN bank_branches ON bank_id = banks.id
+    JOIN cities ON city_id = cities.id
+WHERE city_name = 'Минск'
+GO
+
+/* task 3
+Получить список карточек с указанием имени владельца, баланса и названия банка
+*/
+
+SELECT full_name, bank_cards.balance, bank_name
+FROM bank_cards
+    JOIN accounts ON account_id = accounts.id
+    JOIN clients ON client_id = clients.id
+    JOIN banks ON bank_id = banks.id
+GO
+
+/* task 4
+Показать список банковских аккаунтов у которых баланс не совпадает с 
+суммой баланса по карточкам. В отдельной колонке вывести разницу
+*/
+
+SELECT bank_name, full_name, accounts.balance AS account_balance, SUM(bank_cards.balance) AS cards_total, (accounts.balance - SUM(bank_cards.balance)) as balance_difference
+FROM accounts
+    JOIN bank_cards ON account_id = accounts.id
+    JOIN banks ON bank_id = banks.id
+    JOIN clients ON client_id = clients.id
+GROUP BY bank_name, full_name, accounts.balance
+HAVING accounts.balance - SUM(bank_cards.balance) <> 0
 GO
